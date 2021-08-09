e9e70ccae77c2ae04a145faf4676074b8f87706e
Masha Evtihevich <evtihevich1996@gmail.com>
2021-08-10 00:10:31 +0300

    init: added the tasks

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import UserList from './UserList'
+import Login from './Login'
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <UserList />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
